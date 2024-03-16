0016c18f06daf3b52487c50a76b05096881a4d75
Henr88 <161830010+Henr88@users.noreply.github.com>
2024-03-16 17:59:23 +0200

    Add files via upload
    Bash script for starting virtual environment, receiver.py and wserver.py scripts

---
+++ RunHikingWatchAPP.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to kill processes
+cleanup() 
+{
+    echo "Stopping the web server and receiver..."
+    kill $PID1 $PID2
+    wait $PID1 $PID2 2>/dev/null
+}
+
+# Activate the first virtual environment and start the web server
+source /home/sax/Desktop/webserver/env/bin/activate
+
+python /home/sax/Desktop/webserver/RPi/wserver.py &
+PID1=$!
+
+# Activate the second virtual environment and start the receiver script
+source /home/sax/Desktop/webserver/env/bin/activate
+
+python /home/sax/Desktop/webserver/RPi/receiver.py &
+PID2=$!
+
+# Setup trap for cleanup on CTRL+C
+trap cleanup INT
+
+# Wait for processes to exit
+wait $PID1 $PID2
+
+# Cleanup action
+cleanup
